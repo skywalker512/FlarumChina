23f12d42f9880d5e61883af1141a15c0d06133d9
houzhenhong <houzhenhong@hotmail.com>
2017-02-10 09:01:37 +0800

    fix积分插件导致，用户界面空白

--- vendor/flarum/core/src/Install/Console/InstallCommand.php
+++ vendor/flarum/core/src/Install/Console/InstallCommand.php
@@ -343,6 +343,7 @@ class InstallCommand extends AbstractCommand
             'flarum-auth-github',
             'flarum-auth-twitter',
             'flarum-pusher',
+            'antoinefr-money',
         ];
 
         foreach ($extensions->getExtensions() as $name => $extension) {
