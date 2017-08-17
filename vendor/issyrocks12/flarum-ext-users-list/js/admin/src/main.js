1409bf78843c31841eebdafb9806be324c09a1aa
skywalker512 <houzhenhong@hotmail.com>
2017-08-17 13:32:02 +0800

    del issyrocks12\UsersList

--- vendor/issyrocks12/flarum-ext-users-list/js/admin/src/main.js
+++
@@ -1,6 +0,0 @@
-import app from 'flarum/app';
-import addUsersListPane from 'issyrocks12/users-list/addUsersListPane';
-
-app.initializers.add('issyrocks12-users-list', app => {
-    addUsersListPane();
-});
